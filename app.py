45999fa6a7e93ceaed101636145a8886488fa32a
judybabe <judy1129888@gmail.com>
2021-01-27 20:51:04 +0800

    update4

--- app.py
+++ app.py
@@ -44,6 +44,22 @@ def handle_message(event):
         r = '嗨！簡貝貝'
     elif msg in ['Love you', '愛你', '愛泥', '愛尼', '愛妳']:
         r = '偶也愛你辣'
+    elif msg in ['吃了嗎', '吃飯了嗎', '餓嗎', '寶貝餓了嗎']:
+        r = '餓扁了幫我外送'
+    elif msg in ['飲料', '要喝飲料嗎', '好想喝飲料', '好渴']:
+        r = '奶綠加小芋圓'
+    elif msg in ['早餐', '早餐要吃什麼', 'breakfast']:
+        r = '麵線大辣油像菜沙茶多'
+    elif msg in ['午餐', '午餐要吃什麼', 'lunch']:
+        r = '鍋媽'
+    elif msg in ['晚餐要吃什麼', '晚餐']:
+        r = '卡地夫松阪豬'
+    elif msg in ['軟爛', '軟']:
+        r = '你才軟'
+    elif msg in ['要不要去卡地夫吃飯']:
+        r = '衝'
+    elif msg in ['晚安', '寶貝晚安', '寶貝貝晚安']:
+        r = '簡嘉寶晚安'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
