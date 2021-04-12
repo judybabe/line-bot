3a93f713b72b460a0c8ec8351ccfdd4e25d8b7ce
judybabe <judy1129888@gmail.com>
2021-04-12 23:38:12 +0800

    讀書計畫

--- app.py
+++ app.py
@@ -53,56 +53,26 @@ def handle_message(event):
         return
 
     if msg in ['Hi', 'hi', '嗨', '你好']:                                      # rule-based
-        r = '嗨！簡貝貝'
+        r = '嗨！'
     elif msg in ['Love you', '愛你', '愛泥', '愛尼', '愛妳', '愛你辣']:           # NLP natural language processing
         r = '偶也愛你辣'
     elif msg in ['吃了嗎', '吃飯了嗎', '餓嗎', '寶貝餓了嗎', '寶貝要吃什麼']:
         r = '餓扁了幫我外送'
-    elif msg in ['飲料', '要喝飲料嗎', '好想喝飲料', '好渴']:
-        r = '奶綠加小芋圓'
-    elif msg in ['早餐', 'breakfast']:
-        r = '麵線大辣油像菜沙茶多'
-    elif msg in ['午餐', '午餐要吃什麼', 'lunch']:
-        r = '鍋媽'
     elif msg in ['晚餐要吃什麼', '晚餐']:
-        r = '卡地夫松阪豬'
-    elif msg in ['軟爛', '軟']:
-        r = '你才軟'
-    elif msg in ['要不要去卡地夫吃飯']:
-        r = '衝'
+        r = '火鍋'
     elif msg in ['晚安', '寶貝晚安', '寶貝貝晚安']:
-        r = '簡嘉寶晚安'
+        r = '晚安喔，祝好夢！'
     elif msg in ['寶貝在幹嘛']:
         r = '乾妳屁毛'
     elif msg in ['寶貝', 'baby', '水水']:
         r = '我還是你寶貝嗎'
-    elif msg in ['當然囉', '當然是啊', '當然', '是啊']:
-        r = '那就好小胖呆'
-    elif msg in ['寶貝到囉', '寶貝到家囉', '寶貝到家了']:
-        r = '快ㄑ洗澡澡'
-    elif msg in ['我在騎車', '騎車', '騎車中']:
-        r = '66以下，不然殺人'
-    elif msg in ['我去牽車車', '我去牽車', '我把車牽過來']:
-        r = '好慢吶～～'
-    elif msg in ['排位', '寶貝要不要打排位', '要不要打排位', '寶貝貝要不要打排位']:
-        r = '我不跟銅牌玩誒'
     elif msg in ['好可愛', '好可愛！']:
         r = '你更可愛'
-    elif msg in ['咬你喔']:
-        r = '來咩！！！！'
     elif msg in ['我難過', '我好難過', '我心情不好']:
-        r = '寶貝鼻要難過有我在'
-    elif msg in ['吵架', '來吵架啊', '來吵架']:
-        r = '你很狗屎'
-    elif msg in ['我想你', '我好想你', '寶貝好想你喔', '寶貝好想你']:
-        r = 'me2 快來找我'
-    elif msg in ['寶貝早安']:
+        r = '鼻要難過有我在'
+    elif msg in ['早安']:
         r = '早餐要吃什麼'
-    elif msg in ['早餐要吃什麼']:
-        r = '我先問的你先說辣'
-    elif msg in ['哈哈哈哈']:
-        r = '沒那麼好笑吧'
-    elif msg in ['寶貝想睡了嗎']:
+    elif msg in ['想睡了嗎']:
         sticker_message = StickerSendMessage(
             package_id='1',
             sticker_id='1'
